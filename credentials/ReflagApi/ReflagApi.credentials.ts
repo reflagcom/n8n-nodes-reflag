86af4dcf6cb36454e68990e26470ee29b6054cef
Erik Hughes <erik.hughes@outlook.com>
2025-10-17 14:37:04 +0200

    style: spaces over tabs

--- credentials/ReflagApi/ReflagApi.credentials.ts
+++ credentials/ReflagApi/ReflagApi.credentials.ts
@@ -1,46 +1,46 @@
 import {
-	IAuthenticateGeneric,
-	ICredentialTestRequest,
-	ICredentialType,
-	INodeProperties,
+  IAuthenticateGeneric,
+  ICredentialTestRequest,
+  ICredentialType,
+  INodeProperties,
 } from 'n8n-workflow';
 
 export class ReflagApi implements ICredentialType {
-	name = 'reflagApi';
-	displayName = 'Reflag API';
-	// Uses the link to this tutorial as an example
-	// Replace with your own docs links when building your own nodes
-	documentationUrl = 'https://docs.reflag.com';
+  name = 'reflagApi';
+  displayName = 'Reflag API';
+  // Uses the link to this tutorial as an example
+  // Replace with your own docs links when building your own nodes
+  documentationUrl = 'https://docs.reflag.com';
 
-	properties: INodeProperties[] = [
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: { password: true },
-			default: '',
-		},
-		{
-			displayName: 'URL',
-			name: 'url',
-			type: 'string',
-			default: 'https://app.reflag.com/api',
-		},
-	];
+  properties: INodeProperties[] = [
+    {
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string',
+      typeOptions: { password: true },
+      default: '',
+    },
+    {
+      displayName: 'URL',
+      name: 'url',
+      type: 'string',
+      default: 'https://app.reflag.com/api',
+    },
+  ];
 
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				Authorization: '={{"Bearer " + $credentials.apiKey}}',
-			},
-		},
-	};
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: '={{"Bearer " + $credentials.apiKey}}',
+      },
+    },
+  };
 
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: '={{$credentials.url}}',
-			url: '/apps',
-		},
-	};
+  test: ICredentialTestRequest = {
+    request: {
+      baseURL: '={{$credentials.url}}',
+      url: '/apps',
+    },
+  };
 }
